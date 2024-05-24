a3310991235e5fbad204838b125b202ba01e7600
Sambidh <raisambidh@gmail.com>
2024-05-24 22:31:51 +0545

    custom user model half.

--- leads/models.py
+++ leads/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+# User = get_user_model()
+
+class User(AbstractUser):
+    cellphone_number = models.CharField(max_length=15)
 
 class Lead(models.Model):
     first_name = models.CharField(max_length=20)
@@ -10,5 +16,7 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
 
 class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
+
