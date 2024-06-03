d7dfd73580bf8275fa15b41e038b71ccf2e36b24
Sambidh <raisambidh@gmail.com>
2024-06-03 22:53:00 +0545

    2:32

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Lead
+from .forms import LeadForm
 
 def lead_list(request):
     leads = Lead.objects.all()
@@ -17,4 +18,7 @@ def lead_detail(request, pk):
     return render(request, "leads/lead_detail.html", context)
 
 def lead_create(request):
-    return render(request, "leads/lead_create.html")
+    context = {
+        "form": LeadForm(),
+    }
+    return render(request, "leads/lead_create.html",context)
