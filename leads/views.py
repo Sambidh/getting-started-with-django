d430b8335ee1d2d7458f5d8a03e1bf5d52dd6404
Sambidh <raisambidh@gmail.com>
2024-06-05 23:03:51 +0545

    2.49 Forms and create view completed

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Lead
+from .models import Lead, Agent
 from .forms import LeadForm
 
 def lead_list(request):
@@ -20,8 +20,20 @@ def lead_detail(request, pk):
 def lead_create(request):
     form = LeadForm()
     if request.method == "POST":
-        print('Receiving a post request')
         form = LeadForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            age = form.cleaned_data['age']
+            agent = Agent.objects.first()
+            Lead.objects.create(
+                first_name=first_name,
+                last_name=last_name,
+                age=age,
+                agent=agent
+            )
+            return redirect("/leads")
     context = {
         "form": form,
         }
