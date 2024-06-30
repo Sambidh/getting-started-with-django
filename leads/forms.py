c98b149fd8062380a1a9e84784e0c01591f23513
Sambidh <raisambidh@gmail.com>
2024-06-30 22:43:50 +0545

    authentication basics done. a lot of things can be done here like password reset and such.

--- leads/forms.py
+++ leads/forms.py
@@ -1,5 +1,9 @@
 from django import forms
 from .models import Lead
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class LeadModelForm(forms.ModelForm):
     class Meta:
@@ -8,10 +12,16 @@ class LeadModelForm(forms.ModelForm):
            'first_name',
            'last_name',
            'age',
-           'agent'
+           'agent',
        ) 
 
 class LeadForm(forms.Form):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
     age = forms.IntegerField(min_value=0)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {'username': UsernameField}
